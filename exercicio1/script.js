025fb82ba10e937c1352a7e0977cc172cd151504
joaohgs78 <102187505+joaohgs78@users.noreply.github.com>
2023-02-02 21:42:19 -0300

    resoluçoes-exercicios

---
+++ exercicio1/script.js
@@ -0,0 +1,25 @@
+/* Construa um programa que:
+
+a) Peça ao usuário que insira um número **par**
+
+b) Imprima no console **o resto da divisão** desse número por 2.
+
+c) Teste o programa com diversos números pares. Você notou um padrão? Escreva em um comentário de código.
+
+d) O que acontece no console se o usuário inserir um número ímpar? 
+Escreva em um comentário de código. */
+
+
+
+let numeroPar = prompt('Por favor insira um número par: ')
+
+let divisao = numeroPar % 2
+
+console.log(`O Resto da divisao é : ${divisao}`)
+
+// todo numero par dividido sempre da 0
+
+/* quando inserir numero impar resultado sera sempre 1 */
+
+ 
+
